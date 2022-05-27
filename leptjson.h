7b9fedba61ef17527f208eedb28e72b43d275d87
qsc999 <59675321+csq9329@users.noreply.github.com>
2022-05-27 21:17:49 +0800

    Add files via upload

---
+++ leptjson.h
@@ -0,0 +1,31 @@
+#ifndef LEPTJSON_H__
+#define LEPTJSON_H__
+
+typedef enum{ LEPT_NULL, LEPT_FALSE, LEPT_TRUE, LEPT_NUMBER, LEPT_STRING, LEPT_ARRAY, LEPT_OBJECT } lept_type;
+
+typedef struct {
+	lept_type type;
+}lept_value;
+
+enum {
+    LEPT_PARSE_OK = 0,
+    LEPT_PARSE_EXPECT_VALUE,
+    LEPT_PARSE_INVALID_VALUE,
+    LEPT_PARSE_ROOT_NOT_SINGULAR
+};
+/*
+若一个 JSON 只含有空白，传回 LEPT_PARSE_EXPECT_VALUE。
+若一个值之后，在空白之后还有其他字符，传回 LEPT_PARSE_ROOT_NOT_SINGULAR。
+若值不是那三种字面值，传回 LEPT_PARSE_INVALID_VALUE。
+*/
+int lept_parse(lept_value* v, const char* json);
+
+/* 一般用法
+lept_value v;
+const char json[] = ...;
+int ret = lept_parse(&v, json);
+*/
+//访问结果函数，获取其类型
+lept_type lept_get_type(const lept_value* v);
+
+#endif /* LEPTJSON_H__ */
